823ca5db36af4e64079f0f6c95e3a2e6694bbabb
Siddiqa Badar <siddiqaasim778899@gmail.com>
2025-05-07 02:58:16 +0500

    commit

--- self_task1.py
+++ self_task1.py
@@ -3,22 +3,17 @@
 # Assignment:
 # Create a class Student with attributes name and marks. Use the self keyword to initialize these values via a constructor. Add a method display() that prints student details.
 
-
-
 class Student:
     def __init__(self, name, marks):
         # Initializing instance variables using self
         self.name = name
         self.marks = marks
-
-    def display(self):
-        # Displaying student details using self
-        print(f"Student Name: {self.name} and Marks: {self.marks}")
-       
+      
 # Creating an instance of the Student class and displaying its details
 student1 = Student("Siddiqa Badar", 98)
 student2 = Student("Ali Khan", 85)
 student3 = Student("Sara Ali", 78)
-student1.display()
-student2.display()
-student3.display()
+
+print(f"Student Name: {student1.name}, Marks: {student1.marks}")
+print(f"Student Name: {student2.name}, Marks: {student2.marks}")
+print(f"Student Name: {student3.name}, Marks: {student3.marks}")
