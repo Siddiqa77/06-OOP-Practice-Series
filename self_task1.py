433d3b2274babcc915a19ecdc3eeb6da40edce83
Siddiqa Badar <siddiqaasim778899@gmail.com>
2025-05-07 03:06:29 +0500

    commit new

--- self_task1.py
+++ self_task1.py
@@ -8,12 +8,18 @@ class Student:
         # Initializing instance variables using self
         self.name = name
         self.marks = marks
+
+    def display(self):
+            # Displaying student details
+            print(f"Student Name: {self.name}, Marks: {self.marks}")
       
 # Creating an instance of the Student class and displaying its details
 student1 = Student("Siddiqa Badar", 98)
 student2 = Student("Ali Khan", 85)
 student3 = Student("Sara Ali", 78)
 
-print(f"Student Name: {student1.name}, Marks: {student1.marks}")
-print(f"Student Name: {student2.name}, Marks: {student2.marks}")
-print(f"Student Name: {student3.name}, Marks: {student3.marks}")
+# Displaying details of each student
+student1.display()
+student2.display()
+student3.display()
+
